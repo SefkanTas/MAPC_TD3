d60d63f398241b59e107474ea3e344d0dc16d7cb
Sefkan <tas.sefkan@gmail.com>
2020-10-20 16:51:09 +0200

    add exception

--- src/main/java/exceptions/MonException.java
+++ src/main/java/exceptions/MonException.java
@@ -2,4 +2,21 @@ package exceptions;
 
 public class MonException extends Exception{
 
+    private String message;
+
+    public MonException() {
+        message = "Il y a eu une erreur";
+    }
+
+    public MonException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("MonException{");
+        sb.append("message='").append(message).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
